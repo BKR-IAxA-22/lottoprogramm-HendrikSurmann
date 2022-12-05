41c38586a35d25e7a1a26dc64fa00c957c051e37
Hendrik Surmann <suhriyt@gmail.com>
2022-12-05 10:10:38 +0100

    done

--- Lottoprogramm/Form1.Designer.cs
+++ Lottoprogramm/Form1.Designer.cs
@@ -28,13 +28,197 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button
+            // 
+            this.button.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button.Location = new System.Drawing.Point(0, 0);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(222, 43);
+            this.button.TabIndex = 0;
+            this.button.Text = "Generate";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "123";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "123";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(80, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "123";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(114, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "123";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(148, 59);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(28, 16);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "123";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(182, 59);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(28, 16);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "123";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(182, 85);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(28, 16);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "123";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 121);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(28, 22);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(49, 121);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(28, 22);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(83, 121);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(28, 22);
+            this.textBox3.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(117, 121);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(28, 22);
+            this.textBox4.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(151, 121);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(28, 22);
+            this.textBox5.TabIndex = 12;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(182, 121);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(28, 22);
+            this.textBox6.TabIndex = 13;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(182, 149);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(28, 22);
+            this.textBox7.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(222, 180);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
     }
 }
 
--- Lottoprogramm/Form1.cs
+++ Lottoprogramm/Form1.cs
@@ -16,5 +16,65 @@ namespace Lottoprogramm
         {
             InitializeComponent();
         }
+
+        private int[] lottonumbers = new int[7];
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            lottonumbers = new int[7];
+            for (int i = 0; i < lottonumbers.Length; i++)
+            {
+                int randomNum = rand.Next(1, 49);
+                if (lottonumbers.Contains(randomNum))
+                    i--;
+                else
+                    lottonumbers[i] = randomNum;
+            }
+
+            label1.Text = lottonumbers[0].ToString();
+            label2.Text = lottonumbers[1].ToString();
+            label3.Text = lottonumbers[2].ToString();
+            label4.Text = lottonumbers[3].ToString();
+            label5.Text = lottonumbers[4].ToString();
+            label6.Text = lottonumbers[5].ToString();
+            label7.Text = lottonumbers[6].ToString();
+
+            if (int.TryParse(textBox1.Text, out _) && lottonumbers.Contains(int.Parse(textBox1.Text)))
+                textBox1.BackColor= Color.Green;
+            else
+                textBox1.BackColor = Color.White;
+
+            if (int.TryParse(textBox2.Text, out _) && lottonumbers.Contains(int.Parse(textBox2.Text)))
+                textBox2.BackColor = Color.Green;
+            else
+                textBox2.BackColor = Color.White;
+
+            if (int.TryParse(textBox3.Text, out _) && lottonumbers.Contains(int.Parse(textBox3.Text)))
+                textBox3.BackColor = Color.Green;
+            else
+                textBox3.BackColor = Color.White;
+
+            if (int.TryParse(textBox4.Text, out _) && lottonumbers.Contains(int.Parse(textBox4.Text)))
+                textBox4.BackColor = Color.Green;
+            else
+                textBox4.BackColor = Color.White;
+
+            if (int.TryParse(textBox5.Text, out _) && lottonumbers.Contains(int.Parse(textBox5.Text)))
+                textBox5.BackColor = Color.Green;
+            else
+                textBox5.BackColor = Color.White;
+        
+            if (int.TryParse(textBox6.Text, out _) && lottonumbers.Contains(int.Parse(textBox6.Text)))
+                textBox6.BackColor = Color.Green;
+            else
+                textBox6.BackColor = Color.White;
+
+            if (int.TryParse(textBox7.Text, out _) && lottonumbers.Contains(int.Parse(textBox7.Text)))
+                textBox7.BackColor = Color.Green;
+            else
+                textBox7.BackColor = Color.White;
+
+        }
     }
 }
